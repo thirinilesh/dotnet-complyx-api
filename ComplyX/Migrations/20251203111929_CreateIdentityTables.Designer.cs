de73b622a1ced6f42860def6fa2b783af841f2cf
JeelPatel <92355795+thirinilesh@users.noreply.github.com>
2026-02-09 13:08:48 +0530

    Migration Table change

--- ComplyX/Migrations/20251203111929_CreateIdentityTables.Designer.cs
+++ ComplyX/Migrations/20251203111929_CreateIdentityTables.Designer.cs
@@ -7,12 +7,13 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using AppContext = ComplyX_Businesss.Helper.AppContext;
 
 #nullable disable
 
 namespace ComplyX.Migrations
 {
-    [DbContext(typeof(AppDbContext))]
+    [DbContext(typeof(AppContext))]
     [Migration("20251203111929_CreateIdentityTables")]
     partial class CreateIdentityTables
     {
